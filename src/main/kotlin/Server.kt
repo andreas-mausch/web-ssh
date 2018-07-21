c370ce7d8ba98a633ae80f4c7c170e954e733892
neonew <neonew@gmail.com>
2018-07-21 14:15:29 +0200

    websocket

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -3,11 +3,17 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.http.ContentType
+import io.ktor.http.cio.websocket.CloseReason
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.close
+import io.ktor.http.cio.websocket.readText
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
 import org.slf4j.event.Level.INFO
 
 fun main(args: Array<String>) {
@@ -18,10 +24,26 @@ fun Application.main() {
     install(CallLogging) {
         level = INFO
     }
+    install(WebSockets)
 
     routing {
         get("/") {
             call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+
+        webSocket("/ssh") {
+            while (true) {
+                val frame = incoming.receive()
+                when (frame) {
+                    is Frame.Text -> {
+                        val text = frame.readText()
+                        outgoing.send(Frame.Text("YOU SAID: $text"))
+                        if (text.equals("bye", ignoreCase = true)) {
+                            close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                        }
+                    }
+                }
+            }
         }
     }
 }
