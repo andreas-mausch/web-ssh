a5867d50ab345beddc91e5fb6c94195375cedc9a
neonew <neonew@gmail.com>
2018-07-21 22:26:58 +0200

    handle backspace correctly

--- src/main/resources/webapp/js/ssh.js
+++ src/main/resources/webapp/js/ssh.js
@@ -16,7 +16,20 @@ var vm = new Vue({
                 vm.socket = null;
             };
             this.socket.onmessage = function (event) {
-                vm.consoleOutput += event.data;
+                for (var i = 0; i < event.data.length; i++) {
+                    if (event.data.charCodeAt(i) === 8) {
+                        vm.consoleOutput = vm.consoleOutput.slice(0, -1);
+                    }
+                    else if (event.data.charCodeAt(i) === 27
+                        && i + 2 < event.data.length
+                        && event.data.charAt(i + 1) === '[') {
+                        i += 2;
+                        // TODO handle special characters, cursor positioning and colors
+                    }
+                    else {
+                        vm.consoleOutput += event.data.charAt(i);
+                    }
+                }
             };
             this.socket.onerror = function (event) {
             };
