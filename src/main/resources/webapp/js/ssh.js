e8827f6d1c68941fcaca602c4600651141c8d30e
neonew <neonew@gmail.com>
2018-07-22 00:59:23 +0200

    xterm.js

--- src/main/resources/webapp/js/ssh.js
+++ src/main/resources/webapp/js/ssh.js
@@ -3,80 +3,36 @@ var vm = new Vue({
     data: {
         connectionString: 'ws://localhost:8080/ssh/nuc@nuc',
         socket: null,
-        consoleOutput: ''
+        term: null
+    },
+    mounted: function () {
+        this.term = new Terminal();
+        this.term.open(document.getElementById('terminal'));
+        this.term.on('data', function (data) {
+            vm.send(data);
+        });
     },
     methods: {
         connect: function () {
-            this.consoleOutput = '';
-
             this.socket = new WebSocket(this.connectionString);
             // TODO: error handling ERR_CONNECTION_REFUSED
             this.socket.onopen = function (event) {
+                vm.term.clear();
             };
             this.socket.onclose = function (event) {
                 vm.socket = null;
+                vm.term.kill();
             };
             this.socket.onmessage = function (event) {
-                for (var i = 0; i < event.data.length; i++) {
-                    if (event.data.charCodeAt(i) === 8) {
-                        vm.consoleOutput = vm.consoleOutput.slice(0, -1);
-                    }
-                    else if (event.data.charCodeAt(i) === 27
-                        && i + 2 < event.data.length
-                        && event.data.charAt(i + 1) === '[') {
-                        i += 2;
-                        // TODO handle special characters, cursor positioning and colors
-                    }
-                    else {
-                        vm.consoleOutput += event.data.charAt(i);
-                    }
-                }
+                vm.term.write(event.data);
             };
             this.socket.onerror = function (event) {
             };
         },
-        keyPressed: function (event) {
-            this.send(event.which);
-        },
-        keyUp: function (event) {
-            var specialKey = this.handleSpecialKey(event);
-            if (specialKey !== null) {
-                this.send(specialKey);
-            }
-        },
-        preventShortcuts: function (event) {
-            if (this.handleSpecialKey(event) !== null) {
-                event.preventDefault();
-            }
-        },
-        handleSpecialKey: function (event) {
-            if (!this.connected()) {
-                return null;
-            }
-            if (event.ctrlKey && !event.altKey && !event.shiftKey) {
-                var a = 'A'.charCodeAt(0);
-                var z = 'Z'.charCodeAt(0);
-
-                if (event.keyCode >= a
-                    && event.keyCode <= z) {
-                    return 1 + (event.keyCode - a);
-                }
-            }
-            if (event.which === 8) {
-                return event.which;
-            }
-            return null;
-        },
-        onPaste: function (event) {
-            this.sendString(event.clipboardData.getData('Text'));
-        },
         connected: function () {
             return this.socket != null;
         },
-        send: function (keyCode) {
-            this.sendString(String.fromCharCode(keyCode));
-        },
-        sendString: function (string) {
+        send: function (string) {
             if (this.connected()) {
                 this.socket.send(string);
             }
--- src/main/resources/webapp/templates/index.ftl
+++ src/main/resources/webapp/templates/index.ftl
@@ -1,6 +1,7 @@
 <html>
 <head>
-    <link href="static/main.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="static/main.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/xterm@3.5.1/dist/xterm.css"/>
     <title>Web-SSH</title>
 </head>
 
@@ -8,16 +9,11 @@
     <input v-model="connectionString">
     <button v-on:click="connect">Connect</button>
 
-    <div>
-        <textarea v-model="consoleOutput"
-                  @keydown="preventShortcuts"
-                  @keypress.prevent="keyPressed"
-                  @keyup.prevent="keyUp"
-                  @paste.prevent="onPaste"
-                  class="consoleOutput"></textarea>
+    <div id="terminal">
     </div>
 </div>
 
 <script src="https://unpkg.com/vue@2.5.16"></script>
+<script src="https://unpkg.com/xterm@3.5.1/dist/xterm.js"></script>
 <script src="static/ssh.js"></script>
 </html>
