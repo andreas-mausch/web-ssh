52cef181192d74e02541d255b952c5ecae00b237
neonew <neonew@gmail.com>
2018-07-21 16:17:44 +0200

    SshConnectionString and pass it in the websocket-url

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -2,11 +2,11 @@ import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
-import io.ktor.http.ContentType
 import io.ktor.http.cio.websocket.CloseReason
 import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.close
 import io.ktor.http.cio.websocket.readText
+import io.ktor.locations.Locations
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -18,7 +18,6 @@ import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import org.slf4j.LoggerFactory.getLogger
 import org.slf4j.event.Level.INFO
 
-
 fun main(args: Array<String>) {
     embeddedServer(Netty, port = 8080, module = Application::main).start(wait = true)
 }
@@ -29,18 +28,19 @@ fun Application.main() {
     install(CallLogging) {
         level = INFO
     }
+    install(Locations)
     install(WebSockets)
 
     routing {
         get("/") {
-            call.respondText("Hello World!", ContentType.Text.Plain)
+            call.respondText("Hello World!")
         }
 
-        webSocket("/ssh") {
+        webSocket("/ssh/{connectionString}") {
             logger.info("New client connected")
-            val hostname = readTextFrame(incoming)
-            logger.info("hostname: {}", hostname)
-            Ssh(hostname, "nuc")
+            val connectionString = call.parameters["connectionString"]!!
+            logger.info("hostname: {}", connectionString)
+            Ssh(SshConnectionString(connectionString))
 
             while (true) {
                 val text = readTextFrame(incoming)
--- src/main/kotlin/Ssh.kt
+++ src/main/kotlin/Ssh.kt
@@ -1,11 +1,10 @@
-import io.ktor.http.cio.websocket.Frame
 import net.schmizz.sshj.SSHClient
 import net.schmizz.sshj.connection.channel.direct.Session
 import net.schmizz.sshj.transport.verification.ConsoleKnownHostsVerifier
 import net.schmizz.sshj.transport.verification.OpenSSHKnownHosts
 import java.io.File
 
-class Ssh(hostname: String, username: String) {
+class Ssh(connectionString: SshConnectionString) {
 
     private val shell: Session.Shell
 
@@ -13,10 +12,25 @@ class Ssh(hostname: String, username: String) {
         val sshClient = SSHClient()
         val knownHosts = File(OpenSSHKnownHosts.detectSSHDir(), "known_hosts")
         sshClient.addHostKeyVerifier(ConsoleKnownHostsVerifier(knownHosts, System.console()))
-        sshClient.connect(hostname)
-        sshClient.authPublickey(username)
+        sshClient.connect(connectionString.hostname, connectionString.port)
+        connectionString.username?.let { sshClient.authPublickey(it) }
         val session = sshClient.startSession()
         session.allocateDefaultPTY()
         shell = session.startShell()
+    }
+}
+
+class SshConnectionString(connectionString: String) {
+
+    val hostname: String
+    val username: String?
+    val port: Int = 22
+
+    private val regex = Regex("^([A-Za-z][A-Za-z0-9_]*)(@([A-Za-z][A-Za-z0-9_.]*))?\$")
+
+    init {
+        val matchResult = regex.find(connectionString)!!
+        hostname = matchResult.groups[1]!!.value
+        username = matchResult.groups[3]?.value
     }
 }
